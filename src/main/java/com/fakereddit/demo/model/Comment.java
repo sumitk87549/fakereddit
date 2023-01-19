2ef8e11c6b077338825f41ac441c4abc5f7d72b2
Sumit Kumar <sumitk87549@gmail.com>
2023-01-19 12:57:23 +0530

    Db config and Comment entity added
    Database configured and comment entity code completed

--- src/main/java/com/fakereddit/demo/model/Comment.java
+++ src/main/java/com/fakereddit/demo/model/Comment.java
@@ -1,4 +1,32 @@
 package com.fakereddit.demo.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
+
+    private Instant createdDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
 }
